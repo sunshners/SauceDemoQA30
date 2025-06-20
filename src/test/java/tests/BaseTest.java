0a61dfade27d5f2cb0091bd9b59fb1c6c9003518
sunshners <sim28frvr@gmail.com>
2025-06-20 18:19:54 +0300

    added headless, fix tearDown

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -40,6 +40,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
