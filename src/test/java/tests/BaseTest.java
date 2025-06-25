75774ae96c7df4196caf69560de4d930c9601669
sunshners <sim28frvr@gmail.com>
2025-06-25 18:26:28 +0300

    added headless

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -39,7 +39,11 @@ public class BaseTest {
             options.addArguments("--disable-notifications");
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
-           // options.addArguments("--headless");
+            options.addArguments("--headless");
+
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
